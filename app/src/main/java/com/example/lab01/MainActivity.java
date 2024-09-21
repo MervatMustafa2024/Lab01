7429aa82392d2532774f7750b3d7580e7ae25592
Meme <Meme@MacBook-Pro.local>
2024-09-21 13:34:24 -0400

    Finished step 2

--- app/src/main/java/com/example/lab01/MainActivity.java
+++ app/src/main/java/com/example/lab01/MainActivity.java
@@ -11,12 +11,8 @@ import com.example.lab01.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-  Button btn1;
-  //1- create a varible to refer to the new button
-    Button btn2;
-    // create a var for the textview
-    TextView txt1;
 
+ Button btn1;
     private ActivityMainBinding bindingVar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,51 +22,15 @@ public class MainActivity extends AppCompatActivity {
         bindingVar=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(bindingVar.getRoot());
 
-        btn1= findViewById(R.id.btnHello);
-        //2- assign the new button to the varible btn2
-        btn2= findViewById(R.id.butChange);
-        txt1= findViewById(R.id.txtName);
+
+       btn1=findViewById(R.id.btnHello);
 
 
-
-        btn1.setOnClickListener(view -> {
-            CharSequence text = "Hello toast from Mervat!";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(getApplicationContext() , text, duration);
-            toast.show();
-     });
-        //3- write the response int to setonclicl listner function
-        btn2.setOnClickListener(view -> {
-
-          txt1.setText("My Name is Mervat Mustafa");
-
-        });
+       bindingVar.btnHello.setOnClickListener(view->{
 
 
-        bindingVar.btnStatus.setOnClickListener(view->{
-            String message="";
-
-            if ( bindingVar.chkCoffe.isSelected()==true) {
-                // Checkbox is checked
-                 message+="I like coffee ";
-            } else {
-                // Checkbox is unchecked
-                message+= "I do not like Coffee";
-            }
-
-            if ( bindingVar.swtMilk.isSelected()==true) {
-                // Checkbox is checked
-                message+=", and I like Milk";
-            } else {
-                // Checkbox is unchecked
-                message+=", and I do not like Milk";
-            }
-
-
-            Snackbar snackbar = Snackbar.make(view,message, Snackbar.LENGTH_LONG);
-            snackbar.show();
-        });
-
+               }
+       );
 
 
 
